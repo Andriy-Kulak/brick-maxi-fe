0009281b42ce794e422f0ccb4e0471178b254720
Andriy <andkulak@gmail.com>
2022-08-05 14:43:20 -0500

    refactored & cleaned up code

---
+++ content/index.tsx
@@ -0,0 +1,62 @@
+import brickMaxiLogo from '../public/assets/logo.png'
+import bgArt1 from '../public/assets/bgArt1.png'
+import bgArt2 from '../public/assets/bgArt2s.png'
+import artImg from '../public/assets/artCut.png'
+
+export const images = {
+  brickMaxiLogo,
+  bgArt1,
+  bgArt2,
+  artImg,
+}
+
+export const artistSection = [
+  {
+    key: 1,
+    name: 'Artist 1',
+  },
+  {
+    key: 2,
+    name: 'Artist 2',
+  },
+  {
+    key: 3,
+    name: 'Artist 3',
+  },
+]
+
+export const faqAccordion = [
+  {
+    title: 'How Investments are made?',
+    p: `  Each artwork minted through the platform represents a unique
+investment, with its own set of token holders, P&L, and subsequent
+rewards. As part of our services, we vet and negotiate investment
+oppurtunities, hire day-to-day management, and facilitate the voting
+process. In some cases, we may consolidate funds from multiple mints
+into one investment.`,
+    key: 1,
+  },
+  { title: 'Earning rewards', p: '...tbd', key: 2 },
+  {
+    title: 'Where are communications managed?',
+    p: '...tbd',
+    key: 3,
+  },
+  {
+    title: 'Are token holders required to vote on proposals?',
+    p: '...tbd',
+    key: 4,
+  },
+  {
+    title: 'What happens after an investment is liquidated?',
+    p: '...tbd',
+    key: 5,
+  },
+  {
+    title: 'How do artists apply to Brick Maxi?',
+    p: '...tbd',
+    key: 6,
+  },
+  { title: 'Are there platform fees?', p: '...tbd', key: 7 },
+  { title: 'Contact us', p: '...tbd', key: 8 },
+]
