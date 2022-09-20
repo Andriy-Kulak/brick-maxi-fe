08036c36758914b88fb87ab5f052d2e1aa24da35
Andriy <andkulak@gmail.com>
2022-09-19 21:09:46 -0500

    reverting image fixes

--- components/LandingSection/index.tsx
+++ components/LandingSection/index.tsx
@@ -8,7 +8,6 @@ import { avenirNextCondensed, gillSans, weight } from '../utils/fontConfigs'
 import { motion } from 'framer-motion'
 import { textVariant } from '../../utils/motion'
 import { LogoParamProps } from '../../utils/hooks/useScrollEffects'
-import { NextDesktopBgImg, NextMobileBgImg } from '../BgImages'
 
 type ImageCProps = {
   hide?: boolean
@@ -106,14 +105,6 @@ const LandingSection = ({
         objectPosition="center"
         layout="fill"
       />
-      {/* <NextMobileBgImg
-        alt="bg-art-2"
-        src={images.landingBgMobile}
-        objectFit="cover"
-        objectPosition="center"
-        layout="fill"
-      /> */}
-
       <StyledMain>
         <ImageC
           ref={logoRef}
